5c816cd6c2519158865682e84e057e7ca0b50b53
Ross Bryan <bryanro@gvsu.e.edu>
2018-03-29 16:12:15 -0400

    initial work

---
+++ app/game.py
@@ -0,0 +1,33 @@
+story = """
+Welcome to the adventurous world of Pokemon\n
+Here your goal is to catch as many pokemon that you can, and\n
+train them to become a Pokemon master! As you advance through your story\n
+you will be able to battle Gym Leaders and other trainers to level up your pokemon. \n
+
+"""
+def storyString():
+    return story
+
+
+def askIfNew():
+    prompt = "Press:\n\t1 if you are a new trainer\n\t2 if you are an existing trainer: "
+    ans = input(prompt)
+    if ans == "1":
+        print("Please enter a name to create your Pokemon trainer ")
+        name = input("Trainer Name: ")
+        createUser(name)
+    return
+
+def createUser(input):
+    return
+
+def main():
+    print (storyString())
+    askIfNew()
+if __name__ == "__main__":
+    main()
+
+
+
+
+
