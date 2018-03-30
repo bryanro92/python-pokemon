3eba4eae62f1f687fd15893fe645dfe95e48d505
Ross Bryan <bryanro@gvsu.e.edu>
2018-03-29 23:38:03 -0400

    create new users, and log in as old users

--- app/database.py
+++ app/database.py
@@ -3,7 +3,21 @@ import secrets as db
 def mySqlCon():
     return mysql.connector.connect(user=db.getUserName(), database='pokemon')
 def createUser(name, gen):
-    cursor = mySqlCon().cursor(buffered=True)
-    insertString = "INSERT INTO `trainers` (`tID`, `tName, `tGender, `towns_townID`) VALUES (NULL, %s, %s, '0')"
-    cursor.execute(insertString, (name, gen))
-    return 0
+    con = mySqlCon()
+    cursor = con.cursor()
+    #insertString = "INSERT INTO `trainers` (`tID`, `tName, `tGender`, `towns_townID`) VALUES (25,'bill','m','0');"
+    string = "INSERT INTO `trainers` (`tID`, `tName`, `tGender`, `towns_townID`) VALUES (NULL,'"+name+"', '"+gen+"', '0')"
+    cursor.execute(string)
+    con.commit()
+    message = "Great! \nWelcome to the pokemon world "+name+"."
+    cursor.close()
+    con.close()
+    return message
+
+def currentUser(tID):
+    con = mySqlCon()
+    cursor = con.cursor()
+    cursor.execute("select tName from trainers where tID like "+tID)
+    nameList = cursor.fetchone()
+    name = nameList[0]
+    return name
--- app/game.py
+++ app/game.py
@@ -1,7 +1,7 @@
 import database as db
 import mysql.connector
 
-
+#Global Variables
 story = """
 Welcome to the adventurous world of Pokemon\n
 Here your goal is to catch as many pokemon that you can, and\n
@@ -9,6 +9,7 @@ train them to become a Pokemon master! As you advance through your story\n
 you will be able to battle Gym Leaders and other trainers to level up your pokemon. \n
 
 """
+
 def storyString():
     return story
 
@@ -20,7 +21,18 @@ def askIfNew():
         print("Please enter a name to create your Pokemon trainer ")
         name = input("Trainer Name: ")
         gen = input("Trainer Gender: ")
-        db.createUser(name, gen)
+        print(db.createUser(name, gen))
+        return
+    if ans == "2":
+        name = input("\nPlease enter your trainer ID number: ")
+        print("Welcome back "+db.currentUser(name)+"!\n")
+        print("The journey continues...\n\n")
+        return
+    if ans =="exit":
+        exit(0)
+    else:
+        print("\nType 'exit' to quit or\n")
+        askIfNew()
     return
 
 def mySqlCon():
