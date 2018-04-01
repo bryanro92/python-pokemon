8b13da108ad35eb1c270df5cc015dd8fb15748e2
Ross Bryan <ross785@gmail.com>
2018-04-01 12:58:47 -0400

    fix exit

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from app import game
 def main():
     print(game.storyString())
     game.askIfNew()
-    while game.over == False:
+    while game.getGameOver() == False:
         game.actions()
+    print("Thank you for playing! See you again soon!")
 
 if __name__ == '__main__':
     main()
